fe439f20a68bfb261ddf90de44221831ada1e23c
Kent Gruber <kgruber1@emich.edu>
2017-07-30 15:56:24 -0400

    DSL class
    To hold the main DSL logic for building an object of a specific class.

---
+++ src/argz/dsl.cr
@@ -0,0 +1,17 @@
+module Argz
+
+  class DSL
+    def self.build
+      new.build do |builder|
+        with builder yield builder
+      end
+    end
+
+    def build
+      with self yield self
+      return self
+    end
+    
+    
+  end
+end
