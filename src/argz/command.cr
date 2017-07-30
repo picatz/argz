fc185e7bbc0c1cc47685bab9c3dddc5ab7f2c4cb
Kent Gruber <kgruber1@emich.edu>
2017-07-30 16:45:10 -0400

    Update Command DSL

--- src/argz/command.cr
+++ src/argz/command.cr
@@ -2,12 +2,21 @@ module Argz
 
   class Command < DSL
 
+    def self.build(name : String)
+      new.build do |builder|
+        builder.name name
+        with builder yield builder
+      end
+    end
+
     def initialize
       @name        = String
       @description = String
       @summary     = String
       @short       = String
       @long        = String
+      @required    = false
+      @default     = false
       @examples    = [] of String
     end
     
@@ -15,15 +24,39 @@ module Argz
       { "name": @name,
         "short": @short,
         "long": @long,
+        "required": @required,
+        "default": @default,
         "summary": @summary,
         "description": @description,
         "examples": @examples
       }
     end
+    
+    def default(str : String)
+      @default = str
+    end
+
+    def default?
+      return true if @default
+      false
+    end
+    
+    def required?
+      @required
+    end
+    
+    def required
+      @required = true
+    end
+    
+    def required?
+      @required
+    end
 
     def name(str : String)
       @name = str
     end
+    
     
     def name
       @name
